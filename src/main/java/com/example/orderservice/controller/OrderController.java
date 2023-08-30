1be72e4a9f9de66e8e873512e3a225c633c45680
Vinicius <vinicius.devjvm@gmail.com>
2023-08-30 18:47:43 -0300

    criada a entidade order e seus respectivos controler, service e repository

---
+++ src/main/java/com/example/orderservice/controller/OrderController.java
@@ -0,0 +1,13 @@
+package com.example.orderservice.controller;
+
+import lombok.AllArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/orders")
+@AllArgsConstructor
+@Log4j2
+public class OrderController {
+}
---
+++ src/main/java/com/example/orderservice/entity/Order.java
@@ -0,0 +1,28 @@
+package com.example.orderservice.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.Instant;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Order {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private Long produtoId;
+    private int quantidade;
+    private Instant orderData;
+    private String orderStatus;
+    private Long amount;
+}
